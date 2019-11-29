ea067439066d62d9547f2e9bcaed472cc960e52d
bedenz <bedenz@gmail.com>
2019-11-29 18:08:16 +0700

    bagfix

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as PIXI from 'pixi.js';
 import * as PIXI_CANVAS from 'pixi.js-legacy';
 
 
+//
 
 const createGameWebGL = () => new PIXI.Application({
     width: window.innerWidth-20,
@@ -9,26 +10,36 @@ const createGameWebGL = () => new PIXI.Application({
     backgroundColor: 0x66ccff
 });
 
+//  
 const createGameCanvas = () => new PIXI_CANVAS.Application({
     width: window.innerWidth-100,
     height: window.innerHeight-100,
     backgroundColor: 0x66ccff
 });
 
+
+
 const randomInt = (min:number, max:number):number => Math.floor(min + Math.random() * (max + 1 - min));
 
 
 let game:PIXI.Application;
 
+/*
+    Проверка браузера на совместимость с WebGL, если нет то использовать Canvas
+*/
+
 PIXI.utils.isWebGLSupported() ? game = createGameWebGL() : game = createGameCanvas();
 
+
+/*
+    Массив используемых цветов
+*/
 
 type colorMatrixConfig = number;
 const colorMatrix:colorMatrixConfig[] = [0xff0000, 0x00ff00, 0xff00ff, 0xcccc00, 0x009933, 0xb300b3];
 
 
 document.body.appendChild(game.view);
-
 
 
 class square extends PIXI.Sprite {
@@ -52,7 +63,8 @@ class board extends PIXI.Container {
     private _gameSceneBackground:PIXI.Graphics = new PIXI.Graphics().beginFill(0x383535).drawRect(0,0, 800, 800).endFill();
     
     private _activeSpriteArray:any = []; // буфер, содержит активные объекты
-    private _collisionSquares:any = []; // буфер, содержит одинаковые "соседние" объекты 
+    private _collisionSquares:any = []; // буфер, содержит одинаковые "соседние" объекты
+    private _score:number = 0; //  
 
     constructor() {
         super();
@@ -61,7 +73,6 @@ class board extends PIXI.Container {
         this.position.x = 100;
         this.position.y = 100;
         this._setBackground();
-
     }
 
     private _setBackground() {
@@ -101,6 +112,27 @@ class board extends PIXI.Container {
                         col+=100;                        
                     } 
               }
+        }
+    }
+    public playGame():void {
+        
+        for (let row = 50; row < 800; row+=100) {
+
+            if ( !this.children.some((item:any) => item.x === row && item.y < 150)) {
+
+            const sprite = new square(row, 50 , colorMatrix[randomInt(0,5)]);
+                sprite.on("click",this._onChangePosition, this);
+
+            if (!this.children.slice(1).some((item:any) => 
+                    item.tint === sprite.tint && // цвет 
+                    ((item.y === sprite.y && Math.abs(item.x - sprite.x) === item.width) || // соседи по вертикали
+                    (item.x === sprite.x && Math.abs(item.y - sprite.y) === item.height)))) {
+                
+                this.addChild(sprite);
+            }
+
+            }
+
         }
     }
 
@@ -149,6 +181,14 @@ class board extends PIXI.Container {
         return object1.tint === object2.tint && (object1.position.y === object2.position.y && Math.abs(object1.position.x - object2.position.x) === object1.width || object1.position.x === object2.position.x && Math.abs(object1.position.y - object2.position.y) === object2.height);
     }
 
+    public get score():number {
+        return this._score;
+    }
+/*
+    public set score() {
+        this.score+=1;
+    }
+*/
     public setCollisionSquares():void {
         
         /*
@@ -161,7 +201,10 @@ class board extends PIXI.Container {
         /*
             Метод обходит буфер _collisionSquares и "уничтожает" обьекты
         */
-        this._collisionSquares.forEach((item:any) => this.removeChild(item));
+        this._collisionSquares.forEach((item:any) => {
+            this.removeChild(item);
+            this._score++;
+        });
     }
 
     public checkNextSquareByY(object:square) {
@@ -173,10 +216,17 @@ class board extends PIXI.Container {
     }
 }
 
+class info extends PIXI.Container {
+    constructor() {
+        super();
+    }
+}
+
 const moving = () => {
     gameScene.setCollisionSquares();
     
     gameScene.destroySquares();
+    gameScene.playGame();
     
     gameScene.children.slice(1).forEach((item:any) => {
 
@@ -190,6 +240,7 @@ const moving = () => {
         }
     }
     );
+    console.log(gameScene.score);
 }
 
 const gameScene = new board;
@@ -197,6 +248,7 @@ const gameScene = new board;
 game.ticker.add(moving);
 game.stage.addChild(gameScene);
 gameScene.startGame();
+
 
 
 console.log(gameScene);
